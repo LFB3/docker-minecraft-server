8bd652c413432a944c6e46b1a998a81e8ededbc1
Luca F-B <LFB3@outlook.de>
2025-11-28 19:34:48 +0100

    Fixed Stuff

--- scripts/auto/autopause-daemon.sh
+++ scripts/auto/autopause-daemon.sh
@@ -14,28 +14,26 @@ autopause_error_loop() {
   logAutopause "Please set the environment variable AUTOPAUSE_KNOCK_INTERFACE to the interface that handles incoming connections."
   logAutopause "If unsure which interface to choose, run the ifconfig command in the container."
   logAutopause "Autopause failed to initialize. This log entry will be printed every 30 minutes."
-  while :
-  do
+  while :; do
     sleep 1800
     logAutopause "Autopause failed to initialize."
   done
 }
 
 # wait for java process to be started
-while :
-do
-  if java_process_exists ; then
+while :; do
+  if java_process_exists; then
     break
   fi
   sleep 0.1
 done
 
 # check for interface existence
-if [[ -z "$AUTOPAUSE_KNOCK_INTERFACE" ]] ; then
+if [[ -z "$AUTOPAUSE_KNOCK_INTERFACE" ]]; then
   logAutopause "AUTOPAUSE_KNOCK_INTERFACE variable must not be empty!"
   autopause_error_loop
 fi
-if ! mc-image-helper network-interfaces --check="$AUTOPAUSE_KNOCK_INTERFACE" ; then
+if ! mc-image-helper network-interfaces --check="$AUTOPAUSE_KNOCK_INTERFACE"; then
   logAutopause "Selected interface \"$AUTOPAUSE_KNOCK_INTERFACE\" does not exist!"
   autopause_error_loop
 fi
@@ -47,7 +45,7 @@ fi
 
 /usr/local/sbin/knockd "${knockdArgs[@]}"
 
-if [ $? -ne 0 ] ; then
+if [ $? -ne 0 ]; then
   logAutopause "Failed to start knockd daemon."
   logAutopause "Probable cause: Unable to attach to interface \"$AUTOPAUSE_KNOCK_INTERFACE\"."
   autopause_error_loop
@@ -55,17 +53,16 @@ fi
 
 STATE=INIT
 
-while :
-do
+while :; do
   isTrue "${DEBUG_AUTOPAUSE}" && log "DEBUG: autopause state = $STATE"
   case X$STATE in
   XINIT)
     # Server startup
-    if mc_server_listening ; then
-      TIME_THRESH=$(($(current_uptime)+$AUTOPAUSE_TIMEOUT_INIT))
+    if mc_server_listening; then
+      TIME_THRESH=$(($(current_uptime) + $AUTOPAUSE_TIMEOUT_INIT))
 
-      if [ -e /data/.skip-pause ] ; then
-        logAutopause "'/data/.skip-pause' file is present - skipping pausing"
+      if [ -e /home/container/.skip-pause ]; then
+        logAutopause "'/home/container/.skip-pause' file is present - skipping pausing"
       else
         logAutopause "MC Server listening for connections - pausing in $AUTOPAUSE_TIMEOUT_INIT seconds"
       fi
@@ -75,14 +72,14 @@ do
     ;;
   XK)
     # Knocked
-    if java_clients_connected ; then
+    if java_clients_connected; then
       logAutopause "Client connected - waiting for disconnect"
       STATE=E
-    elif [ -e /data/.skip-pause ] ; then
-      logAutopause "'/data/.skip-pause' file is present - skipping pausing"
+    elif [ -e /home/container/.skip-pause ]; then
+      logAutopause "'/home/container/.skip-pause' file is present - skipping pausing"
       STATE=E
     else
-      if [[ $(current_uptime) -ge $TIME_THRESH ]] ; then
+      if [[ $(current_uptime) -ge $TIME_THRESH ]]; then
         logAutopause "No client connected since startup / knocked - pausing"
         "$(dirname "$0")/pause.sh"
         STATE=S
@@ -91,23 +88,23 @@ do
     ;;
   XE)
     # Established
-    if ! java_clients_connected ; then
-      TIME_THRESH=$(($(current_uptime)+$AUTOPAUSE_TIMEOUT_EST))
+    if ! java_clients_connected; then
+      TIME_THRESH=$(($(current_uptime) + $AUTOPAUSE_TIMEOUT_EST))
       logAutopause "All clients disconnected - pausing in $AUTOPAUSE_TIMEOUT_EST seconds"
       STATE=I
     fi
     ;;
   XI)
     # Idle
-    if java_clients_connected ; then
+    if java_clients_connected; then
       logAutopause "Client reconnected - waiting for disconnect"
       STATE=E
-    elif [ -e /data/.skip-pause ] ; then
-      TIME_THRESH=$(($(current_uptime)+$AUTOPAUSE_TIMEOUT_EST))
-      logAutopause "'/data/.skip-pause' file is present - skipping pausing"
+    elif [ -e /home/container/.skip-pause ]; then
+      TIME_THRESH=$(($(current_uptime) + $AUTOPAUSE_TIMEOUT_EST))
+      logAutopause "'/home/container/.skip-pause' file is present - skipping pausing"
       STATE=E
     else
-      if [[ $(current_uptime) -ge $TIME_THRESH ]] ; then
+      if [[ $(current_uptime) -ge $TIME_THRESH ]]; then
         logAutopause "No client reconnected - pausing"
         "$(dirname "$0")/pause.sh"
         STATE=S
@@ -116,15 +113,15 @@ do
     ;;
   XS)
     # Stopped
-    if rcon_client_exists ; then
+    if rcon_client_exists; then
       "$(dirname "$0")/resume.sh"
     fi
-    if java_running ; then
-      if java_clients_connected ; then
+    if java_running; then
+      if java_clients_connected; then
         logAutopause "Client connected - waiting for disconnect"
         STATE=E
       else
-        TIME_THRESH=$(($(current_uptime)+$AUTOPAUSE_TIMEOUT_KN))
+        TIME_THRESH=$(($(current_uptime) + $AUTOPAUSE_TIMEOUT_KN))
         logAutopause "Server was knocked - waiting for clients or timeout"
         STATE=K
       fi
@@ -134,7 +131,7 @@ do
     logAutopause "Error: invalid state: $STATE"
     ;;
   esac
-  if [[ "$STATE" == "S" ]] ; then
+  if [[ "$STATE" == "S" ]]; then
     # before rcon times out
     sleep 5
   else
